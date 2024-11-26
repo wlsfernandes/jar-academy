1f846f984164c78b55623276b95a00f388ad2264
wlsfernandes@gmail.com <wlsfernandes@gmail.com>
2024-11-26 13:47:49 -0500

    upload vendor scripts, head titlemeta secure_asset to asset

--- resources/views/layouts/vendor-scripts.blade.php
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -1,15 +1,15 @@
 <!-- JAVASCRIPT -->
- <script src="{{ secure_asset('/assets/libs/jquery/jquery.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/bootstrap/bootstrap.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/metismenu/metismenu.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/simplebar/simplebar.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/node-waves/node-waves.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/waypoints/waypoints.min.js')}}"></script>
- <script src="{{ secure_asset('/assets/libs/jquery-counterup/jquery-counterup.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/jquery/jquery.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/bootstrap/bootstrap.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/metismenu/metismenu.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/simplebar/simplebar.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/node-waves/node-waves.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/waypoints/waypoints.min.js')}}"></script>
+ <script src="{{ asset('/assets/libs/jquery-counterup/jquery-counterup.min.js')}}"></script>
 
  @yield('script')
 
  <!-- App js -->
- <script src="{{ secure_asset('/assets/js/app.min.js')}}"></script>
+ <script src="{{ asset('/assets/js/app.min.js')}}"></script>
  
  @yield('script-bottom')

a832d6b7bbbc92fcb083cff58f32b1c300716a83
wlsfernandes <wlsfernandes@gmail.com>
2025-01-24 11:26:29 -0500

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            UserSeeder::class,
+                //UserSeeder::class,
             InstitutionSeeder::class,
             RoleSeeder::class,
             ModulesTableSeeder::class,
